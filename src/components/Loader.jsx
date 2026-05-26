8c7b5e2322f17fed363bb5140a36cb08c28b9c37
Tyler Long <aethdae@gmail.com>
2026-05-26 17:32:04 -0500

    bugfix: image loading

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,7 +4,7 @@ export default function Loader() {
   return (
     <img
       className={spinnerClasses.join(" ") + " w-16 h-auto"}
-      src="src/assets/spinner.png"
+      src="/src/assets/spinner.png"
       alt="loading spinner"
     />
   );
